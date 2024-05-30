08ac2e8f94fb18bbf51040b7733bd9b141984c0d
MayanjaXL <fg-mail@weissglas.com>
2024-05-30 18:46:50 +0300

    Support for data task tools, including retrieving parameters for the connection and basic logging

---
+++ system/data_tasks/data_task_tools.py
@@ -0,0 +1,11 @@
+import sys
+from pathlib import Path
+
+# This assumes that config is at the same level as duft
+root_path = Path(__file__).parent.parent.parent.parent
+sys.path.append("%s/duft" % root_path)
+
+
+def something():
+    print("something")
+    print(sys.path)
--- system/data_tasks/sample_task.py
+++ system/data_tasks/sample_task.py
@@ -1,6 +1,11 @@
 import time
 import json
 import sys
+from data_task_tools import something
+
+something()
+from flask_app.dte.dte_tools import assert_dte_tools_available, get_duft_config, get_resolved_parameters_for_connection, set_data_task_name, log_error, log_message
+
 
 params = {}
 
@@ -16,12 +21,21 @@ if __name__ == "__main__":
     
 
 def sample_task():
+    
+    set_data_task_name("Sample Data Task")
+    resolved_parameters = get_resolved_parameters_for_connection("ANA")
+    log_message("Using parameters: %s" % resolved_parameters)
+    
     for i in range(10):
         # Simulate a long-running task
         time.sleep(params["sleep_time"])
         # Send intermediate update to the client
         
-        print(f'Sample Data Task Progress for %s: {i+1}/10' % params["name"])
-    print('Script completed!')
+        log_message(f'Progress for %s: {i+1}/10' % params["name"])
+    log_message('Script completed!')
         
+
+
+
+assert_dte_tools_available()
 sample_task()
