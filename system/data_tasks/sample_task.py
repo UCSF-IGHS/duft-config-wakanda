b26f61da303a8178516688e51d69d71fb85720d0
MayanjaXL <fg-mail@weissglas.com>
2024-05-31 18:52:48 +0300

    Supporting for logging!

--- system/data_tasks/sample_task.py
+++ system/data_tasks/sample_task.py
@@ -1,29 +1,32 @@
 import time
 import json
 import sys
-from data_task_tools import something
+from data_task_helpers import something
 
 something()
-from src.dte.dte_tools import assert_dte_tools_available, get_duft_config, get_resolved_parameters_for_connection, set_data_task_name, log_error, log_message  # noqa: E402
+from src.dte.dte_tools import assert_dte_tools_available, get_resolved_parameters_for_connection, initialise_data_task, log_error, log_message  # noqa: E402
 
 
 params = {}
 
 if __name__ == "__main__":
+    
+    initialise_data_task("Sample Data Task")
+    
     # Read parameters from the command line argument
     try:
         params = json.loads(sys.argv[1])
         params["name"] is not None
         params["sleep_time"] is not None
     except (IndexError, KeyError):
+        log_error("No parameters given!")
         params = {"name": "No Param Given!!", "sleep_time": 0.2}
         
     
 
 def sample_task():
-    
-    set_data_task_name("Sample Data Task")
     resolved_parameters = get_resolved_parameters_for_connection("ANA")
+    log_message('Script starting!')
     log_message("Using parameters: %s" % resolved_parameters)
     
     for i in range(10):
