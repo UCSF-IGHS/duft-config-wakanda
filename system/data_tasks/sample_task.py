db0e9568c550c6063916f19ccfdd7f0be636e71b
MayanjaXL <fg-mail@weissglas.com>
2024-06-01 16:09:52 +0300

    Logging now part of the DataTaskEnvironment class

--- system/data_tasks/sample_task.py
+++ system/data_tasks/sample_task.py
@@ -4,7 +4,7 @@ import sys
 from data_task_helpers import something
 
 something()
-from src.dte.data_task_tools import assert_dte_tools_available, get_resolved_parameters_for_connection, initialise_data_task, log_error, log_message, find_json_arg, DataTaskEnvironment  # noqa: E402
+from src.dte.data_task_tools import assert_dte_tools_available, get_resolved_parameters_for_connection, initialise_data_task, find_json_arg, DataTaskEnvironment  # noqa: E402
 
 params = {}
 environment: DataTaskEnvironment = None
@@ -18,21 +18,21 @@ if __name__ == "__main__":
     params["sleep_time"] = json_args.get("sleep_time", 0.2)
     
     if not json_args:
-        log_error("No parameters given!")
+        environment.log_error("No parameters given!")
         
 
 def sample_task():
     resolved_parameters = get_resolved_parameters_for_connection("ANA")
-    log_message('Script starting!')
-    log_message("Using Data Connection Parameters: %s" % resolved_parameters)
+    environment.log_message('Script starting!')
+    environment.log_message("Using Data Connection Parameters: %s" % resolved_parameters)
     
     for i in range(10):
         # Simulate a long-running task
         time.sleep(params["sleep_time"])
         # Send intermediate update to the client
         
-        log_message(f'Progress for %s: {i+1}/10' % params["name"])
-    log_message('Script completed! %s' % environment.current_data_task_name)
+        environment.log_message(f'Progress for %s: {i+1}/10' % params["name"])
+    environment.log_message('Script completed! %s' % environment.current_data_task_name)
         
 
 
