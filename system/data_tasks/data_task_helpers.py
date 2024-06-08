fd0430dc67d0e85ea4423baf072d3862333e8053
MayanjaXL <fg-mail@weissglas.com>
2024-06-08 11:59:03 +0300

    Update in data_task_helpers to support the name of the repo (duft_server) instead of just duft

--- system/data_tasks/data_task_helpers.py
+++ system/data_tasks/data_task_helpers.py
@@ -4,6 +4,7 @@ from pathlib import Path
 # This assumes that config is at the same level as duft
 root_path = Path(__file__).parent.parent.parent.parent
 sys.path.append("%s/duft" % root_path)
+sys.path.append("%s/duft-server" % root_path)
 
 
 def something():
