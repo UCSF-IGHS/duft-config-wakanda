bd06e70e820896bce896dc29c76a8f01c491306f
MayanjaXL <fg-mail@weissglas.com>
2024-09-06 11:04:40 +0300

    Sample startup file, which can be used to initialise a country-specific config, including setting custom django database connections.

---
+++ startup.py
@@ -0,0 +1,16 @@
+print("Custom startup loaded")
+from django.conf import settings
+
+# # Override the database engine
+# settings.DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql'
+
+# # You could also override other settings like NAME, USER, PASSWORD, etc.
+# settings.DATABASES['default']['NAME'] = 'my_postgres_db'
+# settings.DATABASES['default']['USER'] = 'my_user'
+# settings.DATABASES['default']['PASSWORD'] = 'my_password'
+# settings.DATABASES['default']['HOST'] = 'localhost'
+# settings.DATABASES['default']['PORT'] = '5432'
+
+# print("Custom database settings applied!")
+
+# print(settings.DATABASES)
