0f1201eae024b69904979b4ed5b8437c15feddfa
MayanjaXL <fg-mail@weissglas.com>
2024-09-07 13:43:41 +0300

    Support for automatic updates of DUFT Config through a data task

---
+++ system/data_tasks/update.py
@@ -0,0 +1,47 @@
+import os
+import sys
+from core.environment import Environment
+from core.update_tools import download_github_repo
+from api_data_task_executioner.data_task_tools import assert_dte_tools_available, get_resolved_parameters_for_connection, initialise_data_task, find_json_arg, DataTaskEnvironment  # noqa: E402
+
+# To try this out, run
+# python update.py '{"repo_url": "https://github.com/UCSF-IGHS/duft-config", "save_path": "weiu34iurer/downloads", "final_repo_name": "duft-config-unittest", "branch": "namibia-3dl"}'
+
+
+params = {}
+environment: DataTaskEnvironment = None
+
+if __name__ == "__main__":
+    
+    json_args = find_json_arg(sys.argv)
+    environment = initialise_data_task("DUFT Config Updater", params=json_args)
+    
+    params["repo_url"] = json_args.get("repo_url")
+    params["save_path"] = json_args.get("save_path")
+    params["final_repo_name"] = json_args.get("final_repo_name")
+    params["branch"] = json_args.get("branch")
+    
+    if not json_args:
+        environment.log_error("No parameters given!")
+        
+def update_config(repo_url, save_path, final_repo_name, branch):
+    environment.log_message('Updating DUFT Reports Configuration')
+    environment.log_message(f'Updating { final_repo_name } from { repo_url }')
+    download_github_repo(repo_url, save_path, final_repo_name=final_repo_name, branch=branch)
+    environment.log_message('Update complete. Please exit and restart DUFT.')
+
+
+
+update_config(params["repo_url"], 
+              params["save_path"], 
+              params["final_repo_name"], 
+              params["branch"])
+
+
+# {
+#   "repo_url": "https://github.com/UCSF-IGHS/duft-config",
+#   "save_path": "/current/directory/weiu34iurer/downloads",
+#   "final_repo_name": "duft-config-unittest",
+#   "branch": "namibia-3dl"
+# }
+
